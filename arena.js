f3c7e2ca95b18a74838da9832718b9c56ff939b6
Kawthara <kawtharr.m.ahmad@gmail.com>
2025-07-01 16:05:08 +0300

    Multiple items can fall and be catched, score is updated

--- arena.js
+++ arena.js
@@ -1,12 +1,19 @@
+// Global Variables
 let score = 0
-let isGameOver = false
-let items = []
-let paw = document.querySelector(".catcher")
+let currentX = 0
+const paw = document.querySelector(".catcher")
+const displayedScore = document.querySelector("#score")
+
+// Remove initial item
+let htmlItems = document.querySelectorAll(".item")
+if (htmlItems[0]) htmlItems[0].remove()
+
+displayedScore.innerText = score
 
 class Items {
-  constructor(type, effect) {
+  constructor(type) {
     this.type = type
-    this.effect = effect
+    this.setScore()
     this.initHTMLElement()
   }
 
@@ -19,85 +26,101 @@ class Items {
         this.score = 5
         break
       case "snakePlant":
-        this.score = 10
+        this.score = -10
         break
       case "onion":
-        this.score = 5
+        this.score = -5
         break
       default:
         this.score = 0
-        break
     }
   }
 
+  // A class function to randomize the new element position from left
   initializePosition() {
-    let left = Math.ceil(Math.random() * 100)
-    left = left + "%"
-    // console.log(left)
-    //let right= Math.ceil(Math) I dont think we would need both left and right
-    return left
+    return Math.ceil(Math.random() * 99) + "%"
   }
-  initHTMLElement() {
-    let parentElement = document.querySelector(".falling-items")
-    let newElement = document.createElement("img")
-    // console.log(this.type + "png")
-    newElement.setAttribute("src", this.type + ".png")
-    newElement.setAttribute("id", "item")
 
-    this.position = this.initializePosition()
-    newElement.style.left = "50%" //it should equal position this is just for initial demonstration
+  // A class function to generate a new element and add it to html
+  initHTMLElement() {
+    const parentElement = document.querySelector(".falling-items")
+    const newElement = document.createElement("img")
+    const animationDuration = Math.ceil(Math.random() * (6 - 3) + 3) + "s"  // min and max should be variable based on user score
+
+    newElement.setAttribute("src", this.type + ".png")
+    newElement.setAttribute("class", "item")
+
+    newElement.style.left = this.initializePosition()
+    newElement.style.animationDuration = animationDuration
 
     parentElement.append(newElement)
+    newElement.addEventListener("animationend", () => {
+      if (catched(newElement)) {
+        console.log("catched") //should be deleted
+        score += this.score
+        displayedScore.innerText = score
+      }
+      removeItem(newElement)
+    })
   }
 }
 
+// A function to randomly create objects -items out of the Items class
 const generateItems = () => {
   let random = Math.ceil(Math.random() * 4)
   let type
-  let effect
-
   switch (random) {
     case 1:
       type = "fish"
+      break
     case 2:
       type = "wool"
-      effect = "good"
       break
     case 3:
       type = "snakePlant"
+      break
     case 4:
       type = "onion"
-      effect = "bad"
       break
   }
-  return new Items(type, effect)
+  return new Items(type)
 }
 
-const catched = () => {
+// Check if item is within the reach of the paw +don't foregt to mention the learning material of getBoundingClientRect
+const catched = (itemToBeCatched) => {
   let pawWidth = paw.getBoundingClientRect().width
   let pawStart = paw.getBoundingClientRect().x
   let pawEnd = pawStart + pawWidth
+  let pawTop =
+    paw.getBoundingClientRect().top - (paw.getBoundingClientRect().top * 0.1) 
 
-  let item = document.querySelector("#item")
-  let itemWidth = item.getBoundingClientRect().width
-  let itemStart = item.getBoundingClientRect().x
-  console.log(itemStart)
+  let itemWidth = itemToBeCatched.getBoundingClientRect().width
+  let itemStart = itemToBeCatched.getBoundingClientRect().x
   let itemEnd = itemStart + itemWidth
-  let itemParent=item.parentElement.getBoundingClientRect().height
+  let itemTop = itemToBeCatched.getBoundingClientRect().top
 
-  let itemTop=item.getBoundingClientRect().top
+  /*console.log("paw Top:" + pawTop)  // things are currently working so they have been commented and should be later deleted
+  console.log("paw start: " + pawStart)
+  console.log("paw end:" + pawEnd)
 
-  if (itemStart >= pawStart && itemEnd <= pawEnd && itemTop<.9*itemParent) {
+  console.log("item top: " + itemTop)
+  console.log("item start: " + itemStart)
+  console.log("item end:" + itemEnd)*/
+
+  if (
+    ((itemStart >= pawStart && itemEnd <= pawEnd) ||
+      (itemStart < pawStart && itemEnd >= pawStart) ||
+      (itemStart <= pawEnd && itemEnd > pawEnd)) &&
+    itemTop >= pawTop
+  ) {
     return true
   } else {
     return false
   }
 }
 
-let currentX = 0
-
+// A function to move the paw -catcher- upon user clicks + don't forget to reference the learning materials
 const movePaw = (direction) => {
-
   if (direction === "right") {
     currentX += 10
   } else if (direction === "left") {
@@ -105,27 +128,27 @@ const movePaw = (direction) => {
   }
 
   paw.style.transform = `translateX(${currentX}px)`
- // console.log("Moved to:", paw.style.transform)
 }
 
-let newItem = generateItems()
-
-let htmlitem = document.querySelector("#item")
-
-const removeItem = () => {
-  htmlitem.remove()
-  newItem = null
-  console.log("worked")
-}
-if (catched) {
- // htmlitem.addEventListener("animationend", removeItem)
- console.log("catched")
+// A function to remove items
+const removeItem = (element) => {
+  element.remove()
+  console.log("deleted")  //should be deleted later
 }
 
-document.addEventListener("keydown", (event) => {
-  if (event.key === "ArrowRight") {
-    movePaw("right")
-  } else if (event.key === "ArrowLeft") {
-    movePaw("left")
+// generate items
+const itemInterval = setInterval(() => {
+  if (score < 0) {
+    clearInterval(itemInterval)
+    console.log("Game Over")  //should be on screen, this is just to ensure it's working
+    return
   }
+
+  generateItems() // items fall independently
+}, 2000)
+
+// Event listeners
+document.addEventListener("keydown", (event) => {
+  if (event.key === "ArrowRight") movePaw("right")
+  else if (event.key === "ArrowLeft") movePaw("left")
 })
